34f22f1d7d2f41912c97cd5ad4ca0ce21c8188a5
ktkk <dezuttereluka@gmail.com>
2021-03-14 17:10:53 +0100

    Added error catching

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
 #include <ctime>
+
+#include <iostream>
+#include <cstdlib>
+
 #include "core/core.hpp"
 
 using namespace vulkat;
@@ -9,11 +13,15 @@ int main(int argc, char **argv){
 	// Create a new core object on the heap
 	Core* pCore{ new Core{ Window{ "WindowName", 1280.f, 720.f } } };
 
-	pCore->TestVulkan(); // Print the number of extensions supported
-	pCore->Run(); // Run the game loop
+	try {
+		pCore->TestVulkan(); // Print the number of extensions supported
+		pCore->Run(); // Run the game loop
+	} catch (const std::exception& e) {
+		std::cerr << "Exception caught: '" << e.what() << "'/n";
+		return EXIT_FAILURE;
+	}
 
 	delete pCore;
 
-
-	return 0;
+	return EXIT_SUCCESS;
 }
