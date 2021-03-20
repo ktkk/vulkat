c04e49455a20201dd751c5ec20c4a4f0a3ea8bcb
ktkk <dezuttereluka@gmail.com>
2021-03-20 23:59:34 +0100

    Added basic class structure

---
+++ src/game/game.hpp
@@ -0,0 +1,18 @@
+#ifndef GAME_HPP
+#define GAME_HPP
+
+namespace vulkat {
+	class Game final {
+	public:
+		Game();
+		~Game();
+
+		Game(const Game& other) = delete;
+		Game& operator=(const Game& other) = delete;
+
+	private:
+
+	}
+}
+
+#endif // GAME_HPP
